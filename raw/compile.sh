c3e15c85b782bf65297f9bba3742d5369ebcfa01
lfsorf <lfsorf@users.noreply.github.com>
2022-05-08 12:12:46 -0700

    adds option to force compilation & minor fixes

--- raw/compile.sh
+++ raw/compile.sh
@@ -7,22 +7,22 @@ input=$(find . -name '*.md' | cut -f2 -d. -)
 
 for file in $input
 do
-	mdsum="<!--$(md5sum .$file.md)-->"
-	if test -f "..$file.html"
-	then
-		# skip if a file remains unchanged
-		htmlsum="$(tail ..$file.html --line 1)"
-		if [[ $mdsum == $htmlsum ]]
-		then
-
-			echo "No changes found on .$file.md"
-			continue
-		fi
-	fi
-	echo "Compiling .$file.md..."
-	pandoc ".$file.md" \
-		--standalone \
-		--css /sakura.css \
-		--output "..$file.html"
-	echo "$mdsum" >> ..$file.html
+    mdhash="<!--$(md5sum .$file.md)-->"
+    if [[ -f "..$file.html" ]] && ! [[ "$1" =~ -{1,2}f(orce)? ]]
+    then
+        # skip if a file remains unchanged
+        compiledhash="$(tail ..$file.html --line 1)"
+        if [[ $mdhash == $compiledhash ]]
+        then
+            echo "No changes found on .$file.md"
+            continue
+        fi
+    fi
+    echo "Compiling .$file.md..."
+    pandoc ".$file.md" \
+        --from commonmark \
+        --standalone \
+        --css /sakura.css \
+        --output "..$file.html"
+    echo "$mdhash" >> ..$file.html
 done
